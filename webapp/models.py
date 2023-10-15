c1f343a2c386f5dead48cd2fef2841996a43aca2
Edabouba01 <Marikoben111@gmail.com>
2023-10-15 09:56:01 -0300

    deuxieme parti

--- webapp/models.py
+++ webapp/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
-#La classe nommer etudiant represente un enseignant
-#un enseignant peut donner plusieur cours dans mon cas 
+ 
 class Etudiant(models.Model):
     nom = models.CharField(max_length=100)
     date_naissance = models.DateField(auto_now=False, auto_now_add=False)
@@ -13,9 +12,10 @@ class Etudiant(models.Model):
     
     def __str__(self):
             return f"{self.nom}, {self.date_naissance}, {self.email}, {self.phone}, {self.matricule}"
-    
+    #Note de cours
 class Cour(models.Model):
     titre = models.CharField(max_length=200)
+    note= models.FloatField(default=11)
     url= models.URLField(max_length=254, default= "www.google.com")
     description= models.TextField(max_length=254, default= " Dans ce cours, nous allons corriger, ensemble, cette mauvaise posture qui est à lorigine de votre stress, de vos maux de tête et linconfort global que vous ressentez.Jai conçu ce cours de programmation pour qu'il soit accessible aux débutants.")
     etudiant = models.ForeignKey(Etudiant, on_delete=models.CASCADE )
